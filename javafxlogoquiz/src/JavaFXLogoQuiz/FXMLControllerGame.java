bd681fcc359614e59448cef8fa78320d4898b7ae
Matthew Mo <matthew_mo@hotmail.co.uk>
2017-03-10 17:00:09 +0000

    Added in other logos, adding imgs to scene WIP

--- javafxlogoquiz/src/JavaFXLogoQuiz/FXMLControllerGame.java
+++ javafxlogoquiz/src/JavaFXLogoQuiz/FXMLControllerGame.java
@@ -34,7 +34,16 @@ public class FXMLControllerGame implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle resource)
     {
-        Logo logoOne = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\javafx_logo_color_1.jpg", 1);
+        Logo logoOne = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\ATP_400x400.jpeg", 2);
+        Logo logoTwo = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Aston_Villa.svg.png", 1);
+        Logo logoThree = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Boots.svg.png", 1);
+        Logo logoFour = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Google_2015_logo.svg.png", 1);
+        Logo logoFive = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Minecraft-logo-transparent-background-ut05tirq.png", 1);
+        Logo logoSix = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Northern_large.jpg", 1);
+        Logo logoSeven = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Premier_League_Logo.svg.png", 1);
+        Logo logoEight = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\Tesco-logo.png", 1);
+        Logo logoNine = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\javafx_logo_color_1.jpg", 1);
+        Logo logoTen = new Logo("C:\\Users\\User\\Documents\\NetBeansProjects\\LogoQuiz\\javafxlogoquiz\\src\\Image\\shield.jpg", 1);
         
         File file = new File(logoOne.Url);
         Image img = null;
